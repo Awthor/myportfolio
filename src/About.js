bacf70cd65183402b3d48f49afc757724f607ffe
Aaron Hawthorne <aaronhawthorne12345@gmail.com>
2020-06-12 12:27:47 +0100

    test

--- src/About.js
+++ src/About.js
@@ -22,37 +22,43 @@ export default function Home() {
                     <Image src={Kodflix} roundedCircle className="About-project-pic" />
                     <h3>Kodflix!</h3>
                     <p>Kodflix is a Netflix clone and this was my first ever project, that was built out using:
+                     <ul>
                         <li> React</li>
                         <li> JavaScript</li>
                         <li> ES6+</li>
                         <li> HTML5</li>
                         <li> CSS3</li>
                         <li> Node & Express</li> 
-                        I used modern features such as Grid (CSS) and stateless components (React Hooks).</p>
+                    </ul>
+                    I used modern features such as Grid (CSS) and stateless components (React Hooks).</p>
                 </Col>
                 <Col xs={12} sm={4} className="person-wrapper">
                     <Image src={Me} roundedCircle className="About-project-pic" />   
                     <h3>DrinkMeApp</h3>
                     <p>DrinkMeApp is a Team Application which is... that was built out using:
+                    <ul>
                         <li> React,</li>
                         <li> JavaScript</li>
                         <li> ES6+</li>
                         <li> HTML5</li>
                         <li> CSS3</li>
                         <li> Node & Express</li> 
+                    </ul>
                         I used modern features such as Grid (CSS) and stateless components (React Hooks).</p>
                 </Col>
                 <Col xs={12} sm={4} className="person-wrapper">
                     <Image src={Kodflix} roundedCircle className="About-project-pic" />
                     <h3>Kodflix</h3>
                     <p>Kodflix is a Netflix clone and this was my first ever project, that was built out using:
+                    <ul>
                         <li> React</li>
                         <li> JavaScript</li>
                         <li> ES6+</li>
                         <li> HTML5</li>
                         <li> CSS3</li>
                         <li> Node & Express</li> 
-                        I used modern features such as Grid (CSS) and stateless components (React Hooks).</p>
+                    </ul>
+                    I used modern features such as Grid (CSS) and stateless components (React Hooks).</p>
                 </Col>
             </Row>
         </Container>
