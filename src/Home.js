ceb06066e563ee9e6bbde5538ef6db514f1823a6
Aaron Hawthorne <aaronhawthorne12345@gmail.com>
2020-06-12 11:56:59 +0100

    removed project

--- src/Home.js
+++ src/Home.js
@@ -18,7 +18,6 @@ export default function Home() {
                                 <Link to="/About">
                                 <Image src={Kodflix} roundedCircle className="project-pic" />
                                 <Image src={Kodflix} roundedCircle className="project-pic" />
-                                <Image src={Kodflix} roundedCircle className="project-pic" />
                                 </Link>
                                 <p>I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff. The secret to doing anything is believing that you can do it. Anything that you believe you can do strong enough, you can do. Anything. As long as you believe. Just go out and talk to a tree. Make friends with it. I guess that would be considered a UFO. A big cotton ball in the sky. Tree trunks grow however makes them happy. In nature, dead trees are just as normal as live trees.</p>
                             </Col>
